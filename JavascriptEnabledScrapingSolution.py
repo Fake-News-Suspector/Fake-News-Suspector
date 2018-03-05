5250d1b28a32fec2a331fc8cc2c24e15a94a2f4e
Priyam Shah <priyamshah112@gmail.com>
2018-03-05 20:32:19 +0530

    Updated copy

---
+++ JavascriptEnabledScrapingSolution.py
@@ -0,0 +1,67 @@
+import sys
+from PyQt4.QtGui import QApplication
+from PyQt4.QtCore import QUrl
+from PyQt4.QtWebKit import QWebPage
+import bs4 as bs
+import urllib.request
+
+class Client(QWebPage):
+    def __init__(self,url):
+        self.app = QApplication(sys.argv)
+        QWebPage.__init__(self)
+        self.loadFinished.connect(self.on_page_load)
+        self.mainFrame().load(QUrl(url))
+        self.app.exec_()
+    def on_page_load(self):
+        self.app.quit()
+url='https://www.yahoo.com/news/ford-temporarily-layoff-2-000-hourly-employees-michigan-142659305--sector.html'
+client_response = Client(url)
+source = client_response.mainFrame().toHtml()
+soup = bs.BeautifulSoup(source,'lxml')
+js_test = soup.find('div',attr={'class','D(ib) Fw(b) Mend(10px) Fz(14px) C($c-fuji-blue-1-a) Cur(p)'})
+print(js_test.text)
+
+
+"""
+my_url='https://www.yahoo.com/news/oklahoma-teachers-might-west-virginia-strike-walk-outs-055624171.html?nf=1'
+from selenium import webdriver
+driver = webdriver.Chrome("C:/chromedriver/chromedriver")
+driver.get(my_url)
+p_element = driver.find_element_by_class_name('D(ib) Fw(b) Mend(10px) Fz(14px) C($c-fuji-blue-1-a) Cur(p)')
+print(p_element.text)
+
+DIFFERENT SOLUTION IN JS
+CASPERJS
+PHANTOMJS
+
+DIFFERENT SOLUTIONS IN PYTHON
+Scraping without JS support:
+import requests
+from bs4 import BeautifulSoup
+response = requests.get(my_url)
+soup = BeautifulSoup(response.text)
+soup.find(id="intro-text")
+# Result:
+<p id="intro-text">No javascript support</p>
+
+Scraping with JS support:
+from selenium import webdriver
+driver = webdriver.PhantomJS()
+driver.get(my_url)
+p_element = driver.find_element_by_id(id_='intro-text')
+print(p_element.text)
+# result:
+'Yay! Supports javascript'
+You can also use Python library dryscrape to scrape javascript driven websites.
+
+Scraping with JS support:
+import dryscrape
+from bs4 import BeautifulSoup
+session = dryscrape.Session()
+session.visit(my_url)
+response = session.body()
+soup = BeautifulSoup(response)
+soup.find(id="intro-text")
+# Result:
+<p id="intro-text">Yay! Supports javascript</p>
+"""
--- NewsSite_Scrape(yahoo_news).py
+++ NewsSite_Scrape(yahoo_news).py
@@ -13,46 +13,46 @@ def main_news():
         soup = BeautifulSoup(plain_text,'lxml')
         for link in soup.findAll('a',{'class':'Fw(b) Fz(20px) Lh(23px) LineClamp(2,46px) Fz(17px)--sm1024 Lh(19px)--sm1024 LineClamp(2,38px)--sm1024 Td(n) C(#0078ff):h C(#000)'}):
             href ="https://www.yahoo.com/" + link.get('href')
-            #headlines = link.string
-            #article =   Article(href)
-            #article.download()
-            #article.html
-            #article.parse()
-            #print(href) #to fetch particular articles link
-            #print(article.title) #to fetch headlines
-            #print(article.text) #to fetch body text of article
-            #print(article.top_image) #to fetch image of news article
+##            #headlines = link.string
+##            article =   Article(href)
+##            article.download()
+##            article.html
+##            article.parse()
+##            print("\t \t \t \t LINK TO NEWS \n"+href +"\n") #to fetch particular articles link
+##            print("\t \t \t \t HEADLINE \n"+article.title+"\n") #to fetch headlines
+##            print("\t \t \t \t CONTENT \n"+article.text+"\n") #to fetch body text of article
+##            print("\t \t \t \t IMAGE LINK \n"+article.top_image+"\n") #to fetch image of news article
             comments_article(href)
                         
 def comments_article(addr):
-    url = addr
-    source_code = requests.get(url)
-    plain_text=source_code.text
-    soup = BeautifulSoup(plain_text,'lxml')
-    
+        url = addr
+        source_code = requests.get(url)
+        plain_text=source_code.text
+        soup = BeautifulSoup(plain_text,'lxml')
+        #print(soup.prettify())
     #For name of commenter
-    for commenter in soup.findAll('div',{'class':'D(ib) Fw(b) Mend(10px) Fz(14px) C($c-fuji-blue-1-a) Cur(p)'}):
-            print(commenter.get_text())
+        for commenter in soup.findAll('div',attrs={'class':'D(ib) Fw(b) Mend(10px) Fz(14px) C($c-fuji-blue-1-a) Cur(p)'}):
+            print("\t \t \t \t COMMENTER  \n"+commenter.get_text()+"\n")
     #For Comments    
-    for comments in soup.findAll('div',{'class':'C($c-fuji-grey-l) Mb(2px) Fz(14px) Lh(20px) Pend(8px)'}):
-            print(comments.get_text())
+        for comments in soup.findAll('div',attrs={'class':'C($c-fuji-grey-l) Mb(2px) Fz(14px) Lh(20px) Pend(8px)'}):
+            print("\t \t \t \t COMMENTS \n"+comments.get_text()+"\n")
     #For Replies
-    for replies in soup.findAll('div',{'class':'C($c-fuji-grey-l) Mb(2px) Fz(14px) Lh(20px) Pend(8px)'}):
-            print(replies.get_text())
+        for replies in soup.findAll('div',attrs={'class':'C($c-fuji-grey-l) Mb(2px) Fz(14px) Lh(20px) Pend(8px)'}):
+            print("\t \t \t \t REPLIES \n"+replies.get_text()+"\n")
     #For Comment Likes
-    for like in soup.findAll('button',{'class':'O(n) Bgc(t) Bdc(t) M(0) P(0) Bd(n) Mend(12px)'}):
-            print(like.get_text())
+        for like in soup.findAll('button',attrs={'class':'O(n) Bgc(t) Bdc(t) M(0) P(0) Bd(n) Mend(12px)'}):
+            print("\t \t \t \t LIKE ON COMMENT \n"+like.get_text()+"\n")
     #For Comment Dislikes
-    for dislike in soup.findAll('button',{'class':'O(n) Bgc(t) Bdc(t) M(0) P(0) Bd(n)'}):
-            print(dislike.get_text())
+        for dislike in soup.findAll('button',attrs={'class':'O(n) Bgc(t) Bdc(t) M(0) P(0) Bd(n)'}):
+            print("\t \t \t \t DISLIKE ON COMMENT \n"+dislike.get_text()+"\n")
     #For News Likes
-    for happy in soup.findAll('span',{'class':'Pstart(3px) Fz(11px) Fw(b) C($c-fuji-green-1-b) Pend(6px)'}):
-            print(happy.get_text())
+        for happy in soup.findAll('span',attrs={'class':'Pstart(3px) Fz(11px) Fw(b) C($c-fuji-green-1-b) Pend(6px)'}):
+            print("\t \t \t \t LIKES ON NEWS \n"+happy.get_text()+"\n")
     #For News Dislikes
-    for sad in soup.findAll('span',{'class':'Pstart(3px) Fz(11px) Fw(b) C($c-fuji-red-2-a)'}):
-            print(sad.get_text())
+        for sad in soup.findAll('span',attrs={'class':'Pstart(3px) Fz(11px) Fw(b) C($c-fuji-red-2-a)'}):
+            print("\t \t \t \t DISLIKE ON NEWS \n"+sad.get_text()+"\n")
     #For News Neutrals    
-    for neutrals in soup.findAll('span',{'class':'Pstart(3px) Fz(11px) Fw(b) C($c-fuji-orange-b) Pend(6px)'}):
-            print(neutrals.get_text())
+        for neutrals in soup.findAll('span',attrs={'class':'Pstart(3px) Fz(11px) Fw(b) C($c-fuji-orange-b) Pend(6px)'}):
+            print("\t \t \t \t NEUTRALS ON NEWS \n"+neutrals.get_text()+"\n")
         #page+=1
 main_news() #trade_spider(page)
