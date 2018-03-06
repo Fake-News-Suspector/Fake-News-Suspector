0de1be591f33acf849d631ee83fa2debfcb9d82e
Priyam Shah <priyamshah112@gmail.com>
2018-03-06 20:11:20 +0530

    pre-final changes

--- NewsSite_Scrape(yahoo_news).py
+++ NewsSite_Scrape(yahoo_news).py
@@ -21,13 +21,11 @@ def main_news():
             article.download()
             article.html
             article.parse()
-            print("\t \t \t \t LINK TO NEWS \n"+href +"\n") #to fetch particular articles link
-            print("\t \t \t \t HEADLINE \n"+article.title+"\n") #to fetch headlines
-            print("\t \t \t \t CONTENT \n"+article.text+"\n") #to fetch body text of article
-            print("\t \t \t \t IMAGE LINK \n"+article.top_image+"\n") #to fetch image of news article
-            comments_article(href)
+            with open(article.title,'w') as t:
+                    t.write("\t \t \t \t LINK TO NEWS \n"+href +"\n"+"\t \t \t \t HEADLINE \n"+article.title+"\n"+"\t \t \t \t CONTENT \n"+article.text+"\n"+"\t \t \t \t IMAGE LINK \n"+article.top_image+"\n")
+            comments_article(href,article.title)
                         
-def comments_article(addr):
+def comments_article(addr,headline):
         
         driver = webdriver.Firefox(executable_path=r'C:\firefoxdriver\geckodriver.exe')
         driver.get(addr)
@@ -35,30 +33,31 @@ def comments_article(addr):
         soup = BeautifulSoup(html,'html.parser')
         driver.close()
         #print(sel_soup.prettify())
-    #For name of commenter
-        for commenter in soup.findAll('div',attrs={'class':'D(ib) Fw(b) Mend(10px) Fz(14px) C($c-fuji-blue-1-a) Cur(p)'}):
-            print("\t \t \t \t COMMENTER  \n"+commenter.get_text()+"\n")
-    #For Comments    
-        for comments in soup.findAll('div',attrs={'class':'C($c-fuji-grey-l) Mb(2px) Fz(14px) Lh(20px) Pend(8px)'}):
-            print("\t \t \t \t COMMENTS \n"+comments.get_text()+"\n")
-    #For Replies
-        for replies in soup.findAll('div',attrs={'class':'C($c-fuji-grey-l) Mb(2px) Fz(14px) Lh(20px) Pend(8px)'}):
-            print("\t \t \t \t REPLIES \n"+replies.get_text()+"\n")
-    #For Comment Likes
-        for like in soup.findAll('button',attrs={'class':'O(n) Bgc(t) Bdc(t) M(0) P(0) Bd(n) Mend(12px)'}):
-            print("\t \t \t \t LIKE ON COMMENT \n"+like.get_text()+"\n")
-    #For Comment Dislikes
-        for dislike in soup.findAll('button',attrs={'class':'O(n) Bgc(t) Bdc(t) M(0) P(0) Bd(n)'}):
-            print("\t \t \t \t DISLIKE ON COMMENT \n"+dislike.get_text()+"\n")
-    #For News Likes
-        for happy in soup.findAll('span',attrs={'class':'Pstart(3px) Fz(11px) Fw(b) C($c-fuji-green-1-b) Pend(6px)'}):
-            print("\t \t \t \t LIKES ON NEWS \n"+happy.get_text()+"\n")
-    #For News Dislikes
-        for sad in soup.findAll('span',attrs={'class':'Pstart(3px) Fz(11px) Fw(b) C($c-fuji-red-2-a)'}):
-            print("\t \t \t \t DISLIKE ON NEWS \n"+sad.get_text()+"\n")
-    #For News Neutrals    
-        for neutrals in soup.findAll('span',attrs={'class':'Pstart(3px) Fz(11px) Fw(b) C($c-fuji-orange-b) Pend(6px)'}):
-            print("\t \t \t \t NEUTRALS ON NEWS \n"+neutrals.get_text()+"\n")
-        #page+=1
+        with open(headline,'a') as t:
+            #For name of commenter
+                for commenter in soup.findAll('div',attrs={'class':'D(ib) Fw(b) Mend(10px) Fz(14px) C($c-fuji-blue-1-a) Cur(p)'}):
+                    t.write("\t \t \t \t COMMENTER  \n"+commenter.get_text()+"\n")
+            #For Comments    
+                for comments in soup.findAll('div',attrs={'class':'C($c-fuji-grey-l) Mb(2px) Fz(14px) Lh(20px) Pend(8px)'}):
+                    t.write("\t \t \t \t COMMENTS \n"+comments.get_text()+"\n")
+            #For Replies
+                for replies in soup.findAll('div',attrs={'class':'C($c-fuji-grey-l) Mb(2px) Fz(14px) Lh(20px) Pend(8px)'}):
+                    t.write("\t \t \t \t REPLIES \n"+replies.get_text()+"\n")
+            #For Comment Likes
+                for like in soup.findAll('button',attrs={'class':'O(n) Bgc(t) Bdc(t) M(0) P(0) Bd(n) Mend(12px)'}):
+                    t.write("\t \t \t \t LIKE ON COMMENT \n"+like.get_text()+"\n")
+            #For Comment Dislikes
+                for dislike in soup.findAll('button',attrs={'class':'O(n) Bgc(t) Bdc(t) M(0) P(0) Bd(n)'}):
+                    t.write("\t \t \t \t DISLIKE ON COMMENT \n"+dislike.get_text()+"\n")
+            #For News Likes
+                for happy in soup.findAll('span',attrs={'class':'Pstart(3px) Fz(11px) Fw(b) C($c-fuji-green-1-b) Pend(6px)'}):
+                    t.write("\t \t \t \t LIKES ON NEWS \n"+happy.get_text()+"\n")
+            #For News Dislikes
+                for sad in soup.findAll('span',attrs={'class':'Pstart(3px) Fz(11px) Fw(b) C($c-fuji-red-2-a)'}):
+                    t.write("\t \t \t \t DISLIKE ON NEWS \n"+sad.get_text()+"\n")
+            #For News Neutrals    
+                for neutrals in soup.findAll('span',attrs={'class':'Pstart(3px) Fz(11px) Fw(b) C($c-fuji-orange-b) Pend(6px)'}):
+                    t.write("\t \t \t \t NEUTRALS ON NEWS \n"+neutrals.get_text()+"\n")
+                #page+=1
 
 main_news() #trade_spider(page)
