38233e525f759234c089e9d7b9ae8f80e924a8b6
Priyam Shah <priyamshah112@gmail.com>
2018-03-07 23:53:50 +0530

    final code with proper formatting

--- NewsSite_Scrape(yahoo_news).py
+++ NewsSite_Scrape(yahoo_news).py
@@ -1,39 +1,58 @@
-import requests
-from bs4 import BeautifulSoup
-from newspaper import Article
-from selenium import webdriver
-
+#For Scraping NewsSite (YAHOO NEWS)
+import requests #Website request library
+from bs4 import BeautifulSoup #Scraping Library
+from newspaper import Article #Scraping News Body
+from selenium import webdriver #For JavaScriptEnabled Sites
+import os # For Making Directory of particular Topic
+import errno #For error checking
+ 
 #def trade_spider(max_pages)
 def main_news():
     #page=1
     #while page <= max_pages:
-        url = 'https://www.yahoo.com/news/us/' #+ str(page)
-        source_code = requests.get(url)
-        plain_text=source_code.text
-        soup = BeautifulSoup(plain_text,'lxml')
-        
-    
-        for link in soup.findAll('a',{'class':'Fw(b) Fz(20px) Lh(23px) LineClamp(2,46px) Fz(17px)--sm1024 Lh(19px)--sm1024 LineClamp(2,38px)--sm1024 Td(n) C(#0078ff):h C(#000)'}):
-        
-            href ="https://www.yahoo.com/" + link.get('href')
-            #headlines = link.string
-            article =   Article(href)
-            article.download()
-            article.html
-            article.parse()
-            with open(article.title,'w') as t:
-                    t.write("\t \t \t \t LINK TO NEWS \n"+href +"\n"+"\t \t \t \t HEADLINE \n"+article.title+"\n"+"\t \t \t \t CONTENT \n"+article.text+"\n"+"\t \t \t \t IMAGE LINK \n"+article.top_image+"\n")
-            comments_article(href,article.title)
+        #For getting various categories of webpage
+        url= {'https://www.yahoo.com/news/us/','https://www.yahoo.com/news/world/',
+                 'https://www.yahoo.com/news/politics/','https://finance.yahoo.com/tech/','https://www.yahoo.com/news/science/',
+                 'https://finance.yahoo.com/','https://www.yahoo.com/news/now-i-get-it',
+                 'https://www.yahoo.com/gma','https://www.yahoo.com/news/originals/'}
+                 #+ str(page)
+
+
+        for i in url:
+                #For creating a directory of each category
+                filename = "C:/YahooNews/"+i.split('/')[-2]+"/"
+                if not os.path.exists(os.path.dirname(filename)):
+                    try:
+                        os.makedirs(os.path.dirname(filename))
+                    except OSError as exc: # Guard against race condition
+                        if exc.errno != errno.EEXIST:
+                            raise
+                source_code = requests.get(i) #Server request for a url
+                plain_text=source_code.text #Getting text file of webpage
+                soup = BeautifulSoup(plain_text,'lxml') #parsing webpage
+                
+            
+                for link in soup.findAll('a',{'class':'Fw(b) Fz(20px) Lh(23px) LineClamp(2,46px) Fz(17px)--sm1024 Lh(19px)--sm1024 LineClamp(2,38px)--sm1024 Td(n) C(#0078ff):h C(#000)'}):
+                
+                    href ="https://www.yahoo.com/" + link.get('href')
+                    #headlines = link.string
+                    article =   Article(href)
+                    article.download()
+                    article.html
+                    article.parse()
+                    with open(filename+"/"+article.title,'w') as t:
+                            t.write("\t \t \t \t LINK TO NEWS \n"+href +"\n"+"\t \t \t \t HEADLINE \n"+article.title+"\n"+"\t \t \t \t CONTENT \n"+article.text+"\n"+"\t \t \t \t IMAGE LINK \n"+article.top_image+"\n")
+                    comments_article(href,article.title,filename)
                         
-def comments_article(addr,headline):
+def comments_article(addr,headline,filename):
         
-        driver = webdriver.Firefox(executable_path=r'C:\firefoxdriver\geckodriver.exe')
-        driver.get(addr)
-        html = driver.execute_script("return document.documentElement.outerHTML")
+        driver = webdriver.Firefox(executable_path=r'C:\firefoxdriver\geckodriver.exe') #Using firefox driver 
+        driver.get(addr) # Calling url
+        html = driver.execute_script("return document.documentElement.outerHTML") #Giving client site call to site for loading javascript and then scraping
         soup = BeautifulSoup(html,'html.parser')
-        driver.close()
+        driver.close() 
         #print(sel_soup.prettify())
-        with open(headline,'a') as t:
+        with open(filename+"/"+headline,'a') as t:
             #For name of commenter
                 for commenter in soup.findAll('div',attrs={'class':'D(ib) Fw(b) Mend(10px) Fz(14px) C($c-fuji-blue-1-a) Cur(p)'}):
                     t.write("\t \t \t \t COMMENTER  \n"+commenter.get_text()+"\n")
